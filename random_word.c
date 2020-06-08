614b33195ec8a0527032a7ee0a0f434091f43524
Victor Malod <mcreaper@hotmail.fr>
2020-06-08 13:19:53 +0200

    mise a jour random word

--- random_word.c
+++ random_word.c
@@ -24,16 +24,16 @@ int main(int argc, char* argv[]) {
 
     printf("a[n] = ");
     for (int i = 0; i < n; i++) {
-        printf("%1.3f | ", a[i]);
+        printf("%1.2f | ", a[i]);
     }
 
-    printf("\nMot correspondant (sans erreurs) = ");
+    printf("\nMot correspondant (sans erreur)    = ");
     for (int i = 0; i < n; i++) {
         char c = (a[i] > 0.5) ? '1' : '0';
         printf("%c", c);
     }
     
-    printf("\nMot correspondant (avec erreurs) = ");
+    printf("\nMot correspondant (avec erreur(s)) = ");
     for (int i = 0; i < n; i++) {
         boolean error = (a[i] > 0.45 && a[i] < 0.55);
         char c = (a[i] > 0.5) ? '1' : '0';
