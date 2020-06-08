e2b9f9ae1ca9a0f435e10b699f8b92530ac90e1a
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-06-08 12:29:19 +0200

    Decode dev 1

--- convolutif_utils.c
+++ convolutif_utils.c
@@ -13,19 +13,19 @@ void grow_word(mot M, int length) {
     M->l += length; //grow length
 }
 
-etat creer_etat() {
-
+etat creer_etat(int name) {
   etat tmp = (etat) malloc(sizeof(etat));
   tmp->trans_0 = NULL;
   tmp->trans_1 = NULL;
+  tmp->name = name;
   return tmp;
 }
 
 etat init_automate(){
-  etat e0 = creer_etat();
-  etat e1 = creer_etat();
-  etat e2 = creer_etat();
-  etat e3 = creer_etat();
+  etat e0 = creer_etat(0);
+  etat e1 = creer_etat(1);
+  etat e2 = creer_etat(2);
+  etat e3 = creer_etat(3);
   e0->trans_0 = e0;
   e0->sortie_0[0] = '0';
   e0->sortie_0[1] = '0';
@@ -57,7 +57,16 @@ etat init_automate(){
   e3->trans_1 = e3;
   e3->sortie_1[0] = '1';
   e3->sortie_1[1] = '0';
-
   return e0;
 
+}
+
+
+chemin creer_chemin(int poids, char *code, int length) {
+  chemin tmp = (chemin) malloc(sizeof(chemin));
+  for (int i = 0; i < length; i ++) {
+    tmp->code[i] = code[i];
+  }
+  tmp->length = length;
+  return tmp;
 }
--- convolutif_utils.h
+++ convolutif_utils.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-#define MAX_WORD_SIZE 256
+#define MAX_WORD_SIZE 16
 
 typedef struct mot_t {
     char code[MAX_WORD_SIZE]; //code binaire
@@ -12,7 +12,17 @@ typedef struct etat_t {
     struct etat_t *trans_1; //etat d'arrivé depuis cet état avec en entrée un 1
     char sortie_0[2]; //sortie de la transition de cet êtat avec en entrée un 0
     char sortie_1[2]; //sortie de la transition de cet êtat avec en entrée un 1
+    int name;
 } * etat;
+
+//pour les chemin, on a juste a sauvegarder l'etat au bout du chemin, sont poid et la mot décodé
+typedef struct chemin_t {
+  etat act;
+  int poids;
+  char code[MAX_WORD_SIZE];
+  int length;
+} *chemin;
+
 
 
 mot init_mot();
@@ -20,3 +30,5 @@ mot init_mot();
 void grow_word(mot M, int length);
 
 etat init_automate();
+
+chemin creer_chemin(int poids, char *code, int length);
