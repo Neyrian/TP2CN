e2ec059490ee394f10a72955b5e608d04b708e86
Victor Malod <mcreaper@hotmail.fr>
2020-06-08 12:47:04 +0200

    random word complété

--- random_word.c
+++ random_word.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef int boolean;
+#define true 1;
+#define false 0;
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Veuillez préciser le nombre de bits a fournir en sortie.\n");
@@ -11,9 +15,29 @@ int main(int argc, char* argv[]) {
     int n = atoi(argv[1]);
     srand(time(NULL));
 
+    float a[n];
+
     for (int i = 0; i < n; i++) {
         float r = (float)rand()/(float)RAND_MAX;
-        char c = (r > 0.5) ? '1' : '0';
+        a[i] = r;
+    }
+
+    printf("a[n] = ");
+    for (int i = 0; i < n; i++) {
+        printf("%1.3f | ", a[i]);
+    }
+
+    printf("\nMot correspondant (sans erreurs) = ");
+    for (int i = 0; i < n; i++) {
+        char c = (a[i] > 0.5) ? '1' : '0';
+        printf("%c", c);
+    }
+    
+    printf("\nMot correspondant (avec erreurs) = ");
+    for (int i = 0; i < n; i++) {
+        boolean error = (a[i] > 0.45 && a[i] < 0.55);
+        char c = (a[i] > 0.5) ? '1' : '0';
+        c = (error) ? ((c == '1') ? '0' : '1') : c;
         printf("%c", c);
     }
 
